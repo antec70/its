cc7cb06847565d85a260cc20cf8846ffe71ad22d
Peter <antec70@yandex.ru>
2020-07-28 13:41:32 +0300

    validate currency

--- models/Сurrency.php
+++ models/Сurrency.php
@@ -9,21 +9,28 @@ use yii\db\ActiveRecord;
 class Сurrency
 {
     public function getCurrency($currency = null){
-
+        $pool = array('AUD','AZN','GBP','AMD','BYN',
+            'BGN','BRL','HUF','HKD','DKK',
+            'USD','EUR','INR','KZT','CAD',
+            'KGS', 'CNY','MDL','NOK', 'PLN',
+            'RON','XDR','SGD', 'TJS', 'TRY',
+            'TMT', 'UZS', 'UAH', 'CZK', 'SEK',
+            'CHF', 'ZAR', 'KRW', 'JPY');
         if ($currency == null){
-            $pool = array('AUD','AZN','GBP','AMD','BYN',
-                        'BGN','BRL','HUF','HKD','DKK',
-                        'USD','EUR','INR','KZT','CAD',
-                        'KGS', 'CNY','MDL','NOK', 'PLN',
-                        'RON','XDR','SGD', 'TJS', 'TRY',
-                        'TMT', 'UZS', 'UAH', 'CZK', 'SEK',
-                        'CHF', 'ZAR', 'KRW', 'JPY');
+
             $set = array_rand($pool,1);
             $currency = $pool[$set];
+        }
+        if (!in_array($currency,$pool)){
+            $error = ['status'=>'-1',
+                'message'=>'Incorrect Currency'];
+            return $error;
+
         }
 
        $allCourse =  file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js');
         $data = json_decode($allCourse,true);
+
         $needed = $data['Valute']["$currency"];
 
         $k = $needed['Value'];
